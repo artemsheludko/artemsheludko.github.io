621eeba38ac71c65fca3f1e34de96d9b404b0475
Artem Sheludko <sheludkoartem@gmail.com>
2016-12-31 17:41:23 +0200

    Add Website

---
+++ assets/js/function.js
@@ -0,0 +1,13 @@
+// Navigation
+$('.navigation').on('click', function(e){
+	e.preventDefault();
+	$(this).addClass('open');
+	$('.sidebar').toggleClass('open');
+
+	if ($('.sidebar').hasClass('open')) {
+		$('.menu-close').on('click', function(e){
+			e.preventDefault();
+			$('.sidebar').removeClass('open');
+		})
+	}
+});	
