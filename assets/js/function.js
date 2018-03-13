f5f3171b0f0c2bf6d2d7b8496f3ebbda544403eb
Artem Sheludko <sheludko.artem@gmail.com>
2018-03-13 16:32:07 +0200

    update function.js

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,9 +1,9 @@
+// Animation
+$('.header-title').addClass('animated bounceInLeft');
+$('.header-subtitle').addClass('animated bounceInUp');
+$('.nav-button, .social-top, footer').addClass('animated fadeIn');
+
 $(document).ready(function() {
-  
-  // Animation
-  $('.header-title').addClass('animated bounceInLeft');
-  $('.header-subtitle').addClass('animated bounceInUp');
-  $('.nav-button, .social-top, footer').addClass('animated fadeIn');
   
   // Sidebar
   $('.navigation').on('click', function(e){
@@ -12,13 +12,13 @@ $(document).ready(function() {
 
   $('.canvas').toggleClass('is-open');
   $('.overlay').addClass('show');
-  
+
   $('#mask').click(function() {
     $('.canvas').removeClass('is-open');
     $('.overlay').removeClass('show');
     $('.navigation').html('<span class="icon-pencil"></span>');
   });
-    
+
     $(window).on("resize", function() {
       var e = $(this);
       if (e.width() >= 991) {
